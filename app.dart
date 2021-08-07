60048a481c9dd57918552e98df627f3a339846e6
dan <dnl.bert.uy@gmail.com>
2021-08-07 13:59:04 +0800

    first commit

---
+++ app.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:campus_alert/homeScreen.dart';
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home:HomeScreen()
+  );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:campus_alert/app.dart';
+import 'package:campus_alert/homeScreen.dart';
+import 'package:flutter/material.dart';
+import 'app.dart';
+main(){
+  runApp(App()
+  );
+}
